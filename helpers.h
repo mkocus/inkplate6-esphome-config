2896c1ab62e7b2b75849aa126dea9c6102a931e6
Marcin Kocoń <mkocon@n7mobile.com>
2022-03-13 13:16:50 +0100

    Better power saving and test graph

--- helpers.h
+++ helpers.h
@@ -23,10 +23,29 @@ float readBattery(esphome::gpio::GPIOSwitch* batteryReadMosfet, esphome::adc::AD
     delay(1);
     adcSensor->set_output_raw(true);
     int adc = adcSensor->sample();
-    if (state){
+    if (state) {
         pin->pin_mode(gpio::FLAG_INPUT);
     } else {
         pin->digital_write(false);
     }
     return double(adc) / 4095 * 1.1 * 3.548133892 * 2;
+}
+
+void initExtended(esphome::mcp23017::MCP23017* extender1, esphome::mcp23017::MCP23017* extender2) {
+    // Set all pins of seconds I/O expander to outputs, low.
+    // For some reason, it draw more current in deep sleep when pins are set as
+    // inputs...
+    for (int i = 0; i < 15; i++) {
+        extender2->pin_mode(i, gpio::FLAG_OUTPUT);
+        extender2->digital_write(i, false);
+    }
+
+    // For same reason, unused pins of first I/O expander have to be also set as
+    // outputs, low.
+    extender1->pin_mode(13, gpio::FLAG_OUTPUT);
+    extender1->pin_mode(14, gpio::FLAG_OUTPUT);
+    extender1->pin_mode(15, gpio::FLAG_OUTPUT);
+    extender1->digital_write(13, false);
+    extender1->digital_write(14, false);
+    extender1->digital_write(15, false); 
 }
