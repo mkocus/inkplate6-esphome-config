ceb0372e5e4acd35d9098df2e69a9d903116a21b
Marcin Kocoń <mkocon@n7mobile.com>
2022-03-13 22:25:13 +0100

    220 uA power consumption in deep sleep; Temporary disabled touchscreen power

--- helpers.h
+++ helpers.h
@@ -30,22 +30,3 @@ float readBattery(esphome::gpio::GPIOSwitch* batteryReadMosfet, esphome::adc::AD
     }
     return double(adc) / 4095 * 1.1 * 3.548133892 * 2;
 }
-
-void initExtended(esphome::mcp23017::MCP23017* extender1, esphome::mcp23017::MCP23017* extender2) {
-    // Set all pins of seconds I/O expander to outputs, low.
-    // For some reason, it draw more current in deep sleep when pins are set as
-    // inputs...
-    for (int i = 0; i < 15; i++) {
-        extender2->pin_mode(i, gpio::FLAG_OUTPUT);
-        extender2->digital_write(i, false);
-    }
-
-    // For same reason, unused pins of first I/O expander have to be also set as
-    // outputs, low.
-    extender1->pin_mode(13, gpio::FLAG_OUTPUT);
-    extender1->pin_mode(14, gpio::FLAG_OUTPUT);
-    extender1->pin_mode(15, gpio::FLAG_OUTPUT);
-    extender1->digital_write(13, false);
-    extender1->digital_write(14, false);
-    extender1->digital_write(15, false); 
-}
